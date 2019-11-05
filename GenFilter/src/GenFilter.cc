b0237ba882f5a200a65cda0bbe6d05c0d2a33b96
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2019-11-05 14:49:46 -0600

    Re-wrote GenFilter for hadronic tau + lepton selections

--- GenFilter/src/GenFilter.cc
+++ GenFilter/src/GenFilter.cc
@@ -3,13 +3,13 @@
 // Package:    GenFilter
 // Class:      GenFilter
 // 
-/**\class GenFilter GenFilter.cc GenAnalyzer/GenFilter/src/GenFilter.cc
+/**\class GenFilter GenFilter.cc Generator/GenFilter/src/GenFilter.cc
 
-Description: [one line class summary]
+   Description: Filters unobserverable events of interest
 
-Implementation:
-[Notes on implementation]
- */
+   Implementation:
+
+*/
 //
 // Original Author:  Nathaniel Odell
 //         Created:  Wed Oct  2 18:48:53 CDT 2013
@@ -51,137 +51,185 @@ using namespace std;
 //
 
 class GenFilter : public EDFilter {
-    public:
-        explicit GenFilter(const ParameterSet&);
-        ~GenFilter();
+public:
+  explicit GenFilter(const ParameterSet&);
+  ~GenFilter();
 
-        static void fillDescriptions(ConfigurationDescriptions& descriptions);
+  static void fillDescriptions(ConfigurationDescriptions& descriptions);
 
-    private:
-        virtual void beginJob() ;
-        virtual bool filter(Event&, const EventSetup&);
-        virtual void endJob() ;
+private:
+  virtual void beginJob() ;
+  virtual bool filter(Event&, const EventSetup&);
+  virtual void endJob() ;
 
-        virtual bool beginRun(Run&, EventSetup const&);
-        virtual bool endRun(Run&, EventSetup const&);
-        virtual bool beginLuminosityBlock(LuminosityBlock&, EventSetup const&);
-        virtual bool endLuminosityBlock(LuminosityBlock&, EventSetup const&);
+  virtual bool beginRun(Run&, EventSetup const&);
+  virtual bool endRun(Run&, EventSetup const&);
+  virtual bool beginLuminosityBlock(LuminosityBlock&, EventSetup const&);
+  virtual bool endLuminosityBlock(LuminosityBlock&, EventSetup const&);
 
-        // ----------member data ---------------------------
+  // ----------member data ---------------------------
+  vector<double> _ptmin;
+  vector<double> _etamax;
+  vector<double> _etamin;
+  vector<int>    _id;
+  unsigned       _naccepted; //how many are needed
+  unsigned       _nhadtaus;  //how many hadronic taus are needed
+  int            _verbose;
 };
 
-GenFilter::GenFilter(const ParameterSet& iConfig)
-{
-    //now do what ever initialization is needed
+GenFilter::GenFilter(const ParameterSet& iConfig):
+  _ptmin(iConfig.getUntrackedParameter<vector<double>>("ptmin"))
+  , _etamax(iConfig.getUntrackedParameter<vector<double>>("etamax"))
+  , _etamin(iConfig.getUntrackedParameter<vector<double>>("etamin"))
+  , _id(iConfig.getUntrackedParameter<vector<int>>("id"))
+  , _naccepted(iConfig.getUntrackedParameter<unsigned>("naccepted", 0))
+  , _nhadtaus(iConfig.getUntrackedParameter<unsigned>("nhadtaus", 0))
+  , _verbose(iConfig.getUntrackedParameter<int>("verbose", 0))
 
+{
+  //now do what ever initialization is needed
+  unsigned sizes = _id.size();
+  if(_ptmin.size() != sizes || _etamin.size() != sizes || _etamax.size() != sizes)
+    throw "GenFilter cut vectors have different sizes!";
+  
+  if(_verbose > 0) {
+    printf("GenFilter::GenFilter --> Cuts for decay products of:\n");
+    for(unsigned i = 0; i < sizes ; ++i)
+      printf("%i: ptmin = %.2f GeV/c, %.2f > eta > %.2f\n",
+	   _id[i], _ptmin[i], _etamax[i], _etamin[i]);
+  }
 }
 
 
 GenFilter::~GenFilter()
 {
 
-    // do anything here that needs to be done at desctruction time
-    // (e.g. close files, deallocate resources etc.)
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
 
 }
 
 // ------------ method called on each new Event  ------------
 bool GenFilter::filter(Event& iEvent, const EventSetup& iSetup)
 {
-    Handle<GenParticleCollection> genParticleColl;
-    iEvent.getByLabel("genParticles", genParticleColl);
+  Handle<GenParticleCollection> genParticleColl;
+  iEvent.getByLabel("genParticles", genParticleColl);
 
-    unsigned leptonCount = 0;
-    unsigned higgsCount = 0;
+  unsigned acceptCount = 0; //counting accepted final state particles
+  unsigned hadTauCount = 0; //counting accepted hadronic taus
+  unsigned particleCount = 0;
 
-    for (GenParticleCollection::const_iterator iGenPart = genParticleColl->begin(); iGenPart != genParticleColl->end(); ++iGenPart) {
-        const GenParticle myParticle = GenParticle(*iGenPart);
+  if(_verbose > 3) 
+    cout << "Printing particle collection for " << iEvent.id() << endl;
+   else if(_verbose > 2)
+     cout << "Printing interesting particles for " << iEvent.id() << endl;
+   else if(_verbose > 1)
+     cout << "Printing accepted particles for " << iEvent.id() << endl;
+   if(_verbose > 1)
+     cout << "index: pdg, status,   pt,    eta,    m,    N(daughters)" << endl;
+  
+  for (GenParticleCollection::const_iterator iGenPart = genParticleColl->begin(); iGenPart != genParticleColl->end(); ++iGenPart) {
+    const GenParticle myParticle = GenParticle(*iGenPart);
+    ++particleCount;
+    if(_verbose > 3)
+      printf("%4u:   %5i,    %2i, %8.2f, %12.2f, %8.2f, %3lu\n",
+	     particleCount, myParticle.pdgId(),
+	     myParticle.status(), myParticle.pt(),
+	     myParticle.eta(), myParticle.mass(),
+	     myParticle.numberOfDaughters());
+    if((myParticle.status() == 1 && myParticle.numberOfDaughters() != 0) ||
+       (myParticle.status() != 1 && myParticle.numberOfDaughters() == 0))
+      printf("Error! Conflicting status! %4u:   %5i,    %2i, %8.2f, %12.2f, %8.2f, %3lu\n",
+	     particleCount, myParticle.pdgId(),
+	     myParticle.status(), myParticle.p4().pt(),
+	     myParticle.eta(), myParticle.mass(),
+	     myParticle.numberOfDaughters());
 
-        // Look for the ttbar pair
-        if (abs(myParticle.pdgId()) == 6 && myParticle.status() == 3) {
+    //do tau check specially
+    if(abs(myParticle.pdgId()) == 15 && //is a tau
+       find(_id.begin(),_id.end(),15) != _id.end()) { //cutting on hadronic taus
 
-            for (unsigned i = 0; i < myParticle.numberOfDaughters(); ++i) {
-                const Candidate *ancestor = myParticle.daughter(i);
+      int nu_index = -1; //check if this tau decays or undergoes FSR, store neutrino index
+      bool hadronic = true; //check that it's a hadronic decay
+      for(unsigned index = 0; index < myParticle.numberOfDaughters(); ++index) {
+	if(abs(myParticle.daughter(index)->pdgId()) == 16) nu_index = index;
+	hadronic &= (abs(myParticle.daughter(index)->pdgId()) != 13 &&
+		     abs(myParticle.daughter(index)->pdgId()) != 11); 
+      }
+      if(!hadronic || nu_index < 0) continue;
 
-                // Look for Higgs
-                if (abs(ancestor->pdgId()) == 25 && ancestor->status() == 3) {
-                    ++higgsCount;
+      //get index of the cuts
+      unsigned id = 0;
+      for(unsigned i = 0; i < _id.size(); ++i)
+	if(_id[i] == 15) id = i;
+      
+      auto lv = myParticle.p4() - myParticle.daughter(nu_index)->p4(); //assume the hadronic tau LV = tau - tau neutrino
+      if(lv.pt() > _ptmin[id] &&
+	 lv.eta() > _etamin[id] &&
+	 lv.eta() < _etamax[id]) {
+	++hadTauCount;
+	if(_verbose > 1) {
+	  printf("%4u: %5i,   %2i, %8.2f, %12.2f, %8.2f, %3lu (Accepted)\n Tau daughters pdgs:",
+		 particleCount,
+		 myParticle.pdgId(), myParticle.status(), myParticle.pt(),
+		 myParticle.eta(), myParticle.mass(),
+		 myParticle.numberOfDaughters());
+	  for(unsigned index = 0; index < myParticle.numberOfDaughters(); ++index)
+	    cout << " " << myParticle.daughter(index)->pdgId();
+	  cout << endl;
+	}
+      } else if(_verbose > 2) {
+	printf("%4u:   %5i,    %2i, %8.2f, %12.2f, %8.2f, %3lu (Rejected)\n Tau daughters pdgs:",
+	       particleCount,
+	       myParticle.pdgId(), myParticle.status(), myParticle.pt(),
+	       myParticle.eta(), myParticle.mass(),
+	       myParticle.numberOfDaughters());
+	for(unsigned index = 0; index < myParticle.numberOfDaughters(); ++index)
+	  cout << " " << myParticle.daughter(index)->pdgId();
+	cout << endl;
+      } 
+    }  
 
-                    for (unsigned i = 0; i < ancestor->numberOfDaughters(); ++i) {
-                        const Candidate *ancestor2 = ancestor->daughter(i);
-
-                        // Looking for Ws
-                        if (abs(ancestor2->pdgId()) == 24 && ancestor2->status() == 3) {
-
-                            for (unsigned i = 0; i < ancestor2->numberOfDaughters(); ++i) {
-                                const Candidate *ancestor3 = ancestor2->daughter(i);
-
-                                // Looking for leptons from W
-                                if (
-                                        abs(ancestor3-> pdgId()) == 11 
-                                        || abs(ancestor3-> pdgId()) == 13 
-                                        || abs(ancestor3-> pdgId()) == 15 
-                                   ) {
-                                    //cout  << "\t"<< ancestor3->pdgId() << endl;
-                                    ++leptonCount;
-                                }
-                            }
-                        // Looking for Zs
-                        } else if (abs(ancestor2->pdgId()) == 23 && ancestor2->status() == 3) {
-
-                            for (unsigned i = 0; i < ancestor2->numberOfDaughters(); ++i) {
-                                const Candidate *ancestor3 = ancestor2->daughter(i);
-
-                                // Looking for leptons from Z
-                                if (
-                                        abs(ancestor3-> pdgId()) == 11 
-                                        || abs(ancestor3-> pdgId()) == 13 
-                                        || abs(ancestor3-> pdgId()) == 15 
-                                   ) {
-                                    //cout  << "\t"<< ancestor3->pdgId() << endl;
-                                    ++leptonCount;
-                                }
-                            }
-                        // Looking for taus 
-                        } else if (abs(ancestor2->pdgId()) == 15 && ancestor2->status() == 3) {
-                            ++leptonCount;
-                        }
-                    }
-                } else if (ancestor->status() == 3) { // Should be Ws and bs
-
-                    // Get the leptons from the W decay
-                    if (abs(ancestor->pdgId()) == 24) {
-
-                        for (unsigned i = 0; i < ancestor->numberOfDaughters(); ++i) {
-                            const Candidate *ancestor2 = ancestor->daughter(i);
-
-                            // Looking for leptons
-                            if (
-                                        abs(ancestor2-> pdgId()) == 11 
-                                        || abs(ancestor2-> pdgId()) == 13 
-                                        || abs(ancestor2-> pdgId()) == 15 
-                               ) {
-                                //cout  << "\t"<< ancestor2->pdgId() << endl;
-                                ++leptonCount;
-                            }
-                        }
-                    } 
-                }
-            } 
-        }
-    }
+      
+    //final particles
+    if(myParticle.status() == 1) {
+      int id = myParticle.pdgId();
+      //check if particle of interest
+      if(find(_id.begin(),_id.end(),abs(id)) == _id.end()) continue;
+      unsigned index = 0;
+      for(unsigned i = 0; i < _id.size(); ++i) {
+	if(_id[i] == abs(id)) {
+	  index = i;
+	  break;
+	}
+      }
+      if(myParticle.pt()  > _ptmin[index] &&
+	 myParticle.eta() > _etamin[index] &&
+	 myParticle.eta() < _etamax[index]) {
+	++acceptCount;
+	if(_verbose > 1)
+	  printf("%4u:   %5i,    %2i, %8.2f, %12.2f, %8.2f, %3lu (Accepted)\n",
+		 particleCount,
+		 myParticle.pdgId(), myParticle.status(), myParticle.pt(),
+		 myParticle.eta(), myParticle.mass(),
+		 myParticle.numberOfDaughters());
+      } else if(_verbose > 2)
+	printf("%4u:   %5i,    %2i, %8.2f, %12.2f, %8.2f, %3lu (Rejected)\n",
+	       particleCount,
+	       myParticle.pdgId(), myParticle.status(), myParticle.pt(),
+	       myParticle.eta(), myParticle.mass(),
+	       myParticle.numberOfDaughters());
+      
+    }  
+  }
+  
 
 
-    //return false;
-    if (leptonCount > 1 && higgsCount == 1) {
-        //cout << leptonCount << endl;
-        return true;
-    } else
-        return false;
+  return (acceptCount >= _naccepted) && (hadTauCount >= _nhadtaus);
 }
 
 // ------------ method called once each job just before starting event loop  ------------
-    void 
+void 
 GenFilter::beginJob()
 {
 }
@@ -192,41 +240,41 @@ GenFilter::endJob() {
 }
 
 // ------------ method called when starting to processes a run  ------------
-    bool 
+bool 
 GenFilter::beginRun(Run&, EventSetup const&)
 { 
-    return true;
+  return true;
 }
 
 // ------------ method called when ending the processing of a run  ------------
-    bool 
+bool 
 GenFilter::endRun(Run&, EventSetup const&)
 {
-    return true;
+  return true;
 }
 
 // ------------ method called when starting to processes a luminosity block  ------------
-    bool 
+bool 
 GenFilter::beginLuminosityBlock(LuminosityBlock&, EventSetup const&)
 {
-    return true;
+  return true;
 }
 
 // ------------ method called when ending the processing of a luminosity block  ------------
-    bool 
+bool 
 GenFilter::endLuminosityBlock(LuminosityBlock&, EventSetup const&)
 {
-    return true;
+  return true;
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
 GenFilter::fillDescriptions(ConfigurationDescriptions& descriptions) {
-    //The following says we do not know what parameters are allowed so do no validation
-    // Please change this to state exactly what you do use, even if it is no parameters
-    ParameterSetDescription desc;
-    desc.setUnknown();
-    descriptions.addDefault(desc);
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
 }
 //define this as a plug-in
 DEFINE_FWK_MODULE(GenFilter);
